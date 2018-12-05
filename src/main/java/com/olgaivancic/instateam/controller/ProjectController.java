53f6a3ee1f1f1c2334d7503280b3cb5a067ecdab
Olga Ivancic <olgap2015@users.noreply.github.com>
2018-12-05 15:11:50 +0100

    Roles.html page is set up

--- src/main/java/com/olgaivancic/instateam/controller/ProjectController.java
+++ src/main/java/com/olgaivancic/instateam/controller/ProjectController.java
@@ -1,4 +1,17 @@
 package com.olgaivancic.instateam.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ProjectController {
+
+    // Home page - index of all of the projects
+    @RequestMapping("/")
+    public String listProjects(Model model) {
+        // TODO: add attributes to the model and fix the Index file accordingly
+
+        return "project/index";
+    }
 }
--- src/main/java/com/olgaivancic/instateam/controller/RoleController.java
+++ src/main/java/com/olgaivancic/instateam/controller/RoleController.java
@@ -1,4 +1,48 @@
 package com.olgaivancic.instateam.controller;
 
+import com.olgaivancic.instateam.model.Role;
+import com.olgaivancic.instateam.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
 public class RoleController {
+
+    @Autowired
+    private RoleService roleService;
+
+    @RequestMapping("/roles")
+    public String listRoles(Model model) {
+        List<Role> roles = roleService.findAll();
+        model.addAttribute("roles", roles);
+        return "role/roles";
+    }
+
+    @RequestMapping("/roles/{role.id}/edit")
+    public String editRole(@PathVariable Long roleId, Model model) {
+        // TODO: find the role to edit
+        Role role  = roleService.findById(roleId);
+        model.addAttribute("role", role);
+
+        // TODO: redesign roles_edit file
+        return "role/roles_edit";
+    }
+
+    @RequestMapping(value = "/roles/new", method = RequestMethod.POST)
+    public String addNewRole(Model model) {
+        // TODO: validate the data
+
+        // TODO: save the new role
+
+        // TODO: redirect to the roles view page
+        return "redirect:/roles";
+    }
+
+
 }
