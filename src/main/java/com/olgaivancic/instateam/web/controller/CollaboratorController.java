a57285ba94658aab5f758ed9e86cfd3fcdeb24a4
Olga Ivancic <olgap2015@users.noreply.github.com>
2018-12-12 11:49:55 +0100

    Adding new collaborators is implemented

--- src/main/java/com/olgaivancic/instateam/web/controller/CollaboratorController.java
+++ src/main/java/com/olgaivancic/instateam/web/controller/CollaboratorController.java
@@ -4,11 +4,16 @@ import com.olgaivancic.instateam.model.Collaborator;
 import com.olgaivancic.instateam.model.Role;
 import com.olgaivancic.instateam.service.CollaboratorService;
 import com.olgaivancic.instateam.service.RoleService;
+import com.olgaivancic.instateam.web.FlashMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -31,6 +36,26 @@ public class CollaboratorController {
             model.addAttribute("collaborator", new Collaborator());
         }
         return "collaborator/collaborators";
+    }
+
+    // Post method to save a new collaborator
+    @RequestMapping(value = "/collaborators/new", method = RequestMethod.POST)
+    public String addCollaborator(@Valid Collaborator collaborator, BindingResult result, RedirectAttributes redirectAttributes) {
+        // TODO: Add data validation
+        if (result.hasErrors()) {
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.collaborator", result);
+            redirectAttributes.addFlashAttribute("collaborator", collaborator);
+            return "redirect:/collaborators";
+        }
+
+        System.out.println(collaborator);
+
+        collaboratorService.save(collaborator);
+
+        // Flash message that the save was a success
+        redirectAttributes.addFlashAttribute("flash", new FlashMessage("Collaborator is successfully saved!", FlashMessage.Status.SUCCESS));
+
+        return "redirect:/collaborators";
     }
 
 }
